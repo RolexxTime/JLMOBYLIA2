60613db02dd48d1ee0ed9595eec3d2e5e419014b
RolexxTime <dindjipierre77@gmail.com>
2023-10-20 16:42:00 +0200

    big changes

--- javascript/script.js
+++ javascript/script.js
@@ -47,3 +47,13 @@ const observer = new IntersectionObserver(handleIntersect, options)
 document.querySelectorAll('.reveal').forEach(function (r){
     observer.observe(r)
 })
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
